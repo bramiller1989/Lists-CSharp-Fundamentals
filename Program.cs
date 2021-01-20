3289704b0922b9fb8f63406b75ffacf7a934efb1
bramiller1989 <bramiller1989@gmail.com>
2021-01-20 15:34:29 -0600

    create list and iterate

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lists
 {
@@ -6,7 +7,15 @@ namespace Lists
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new List<int>() {1, 2, 3, 4};
+            numbers.Add(1);
+            numbers.AddRange(new int[3] {5, 6, 7});
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+            Console.WriteLine();
+            Console.WriteLine("Index of 1: " +  numbers.IndexOf(1));
         }
     }
 }
